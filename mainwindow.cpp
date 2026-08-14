4544d587b01309d9c79e5ccb7e7313dc64c396c5
Konstatnin <noskovkg@yandex.ru>
2026-08-14 16:59:35 +0500

    Первые кнопки и вывод текста

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <qstring.h>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +14,22 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_btn_hello_clicked(){
+    if (ui->le_name->text() == QString("")){
+        auto result = QMessageBox::question(this, "Внимание!", "Ты хлебушек?");
+
+        if (result == QMessageBox::StandardButton::Yes){
+            QMessageBox::information(this, "Хуясе", "зовите MEEEEEDIIIIC!");
+        }else if (result == QMessageBox::StandardButton::No){
+            QMessageBox::information(this, "Странно", "А че тупишь?");
+        }else {
+            QMessageBox::information(this, "...", "Ну и хуй с тобой...");
+        }
+    }else{
+        ui->lbl_greetings->setText("Hello, " + ui->le_name->text() + "!");
+    }
+}
+
+
